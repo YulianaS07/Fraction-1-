6443c2bca1bd431f80c315da1b3e591b622f9e21
yulia <yulia@YULLIA>
2024-07-09 15:12:06 +0200

    Add project files.

---
+++ WseiLab1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WseiLab1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Ulamek a = new Ulamek(2, 4);
+            Ulamek b = new Ulamek(5, 8);
+            Ulamek c = new Ulamek(1, 3);
+            Ulamek e = new Ulamek(7, 5);
+
+            Ulamek f = a.Uprosc();
+            Console.WriteLine("uproszczony ułamek to " + f.ToString());
+
+            Ulamek d = a + c;
+
+            Console.WriteLine("wyświetl ułamek d => " + d.ToString());
+
+            if (c == e)
+                Console.WriteLine("tak to jest c = e");
+            else
+                Console.WriteLine("nie to nie jest c = e");
+
+            Console.WriteLine((a + b).ToString());
+            Console.WriteLine((c / b).ToString());
+
+            Ulamek[] tablicaUlamkow = Ulamek.LosoweUlamki(10);
+            Ulamek.PrintArray(tablicaUlamkow);
+            Array.Sort(tablicaUlamkow);
+            Ulamek.PrintArray(tablicaUlamkow);
+
+            System.Threading.Thread.Sleep(20000);
+        }
+
+
+        
+    }
+}
---
+++ WseiLab1/Ulamek.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WseiLab1
+{
+    internal class Ulamek : IComparable<Ulamek>
+    {
+        int licznik;
+        int mianownik;
+
+        public Ulamek Uprosc()
+        {
+            int gcd = ZnajdzGCD(licznik, mianownik);
+            if (gcd == 1)
+                return this;
+            else
+            {
+                licznik /= gcd;
+                mianownik /= gcd;
+                return new Ulamek(licznik, mianownik);
+            }
+        } 
+
+        public Ulamek(int inLicznik, int inMianownik)
+        {
+            if (inMianownik == 0)
+                throw new ArgumentException("Mianownik nie może wynosić 0!");
+            licznik = inLicznik;
+            mianownik = inMianownik;
+        }
+
+
+        public static Ulamek[] LosoweUlamki(int ilosc)
+        {
+            Ulamek[] tablicaUlamkow = new Ulamek[ilosc];
+            Random rnd = new Random(); 
+
+            for (int i = 0; i < ilosc; i++)
+            {
+                tablicaUlamkow[i] = new Ulamek(rnd.Next(10) + 1, rnd.Next(10) + 1); 
+            }
+            return tablicaUlamkow;
+        }
+
+        public static void PrintArray(Ulamek[] array)
+        {
+            foreach (var ulamek in array)
+            {
+                Console.Write(ulamek + " ");
+            }
+            Console.WriteLine(); 
+        }
+        public override string ToString()
+        {
+            return licznik.ToString() + "/" + mianownik.ToString();
+        }
+
+        public int CompareTo(Ulamek other)
+        {
+            double thisValue = (double)licznik/mianownik;
+            double otherValue = (double)other.licznik/other.mianownik;
+
+            return thisValue.CompareTo(otherValue);
+        }
+
+        public static Ulamek operator *(Ulamek valueA, Ulamek valueB)
+        {
+            Ulamek wynik = new Ulamek(valueA.licznik * valueB.licznik, valueA.mianownik * valueB.mianownik);
+            return wynik;
+        }
+
+        public static Ulamek operator /(Ulamek valueA, Ulamek valueB)
+        {
+            Ulamek wynik = new Ulamek(valueA.licznik * valueB.mianownik, valueA.mianownik * valueB.licznik);
+            return wynik;
+        }
+
+        public static Ulamek operator +(Ulamek valueA, Ulamek valueB)
+        {
+            Ulamek wynik = new Ulamek(1, 1);
+            wynik.mianownik = ZnajdzLCM(valueA.mianownik, valueB.mianownik);
+            wynik.licznik = ((wynik.mianownik / valueA.mianownik) * valueA.licznik) + ((wynik.mianownik / valueB.mianownik) * valueB.licznik);
+            return wynik;
+        }
+
+        public static Ulamek operator -(Ulamek valueA, Ulamek valueB)
+        {
+            Ulamek wynik = new Ulamek(1, 1);
+            wynik.mianownik = ZnajdzLCM(valueA.mianownik, valueB.mianownik);
+            wynik.licznik = ((wynik.mianownik / valueA.mianownik) * valueA.licznik) - ((wynik.mianownik / valueB.mianownik) * valueB.licznik);
+            return wynik;
+        }
+
+        public static bool operator !=(Ulamek valueA, Ulamek valueB)
+        {
+            if (valueA.ToString() != valueB.ToString())
+                return true;
+            else
+                return false;
+        }
+        public static bool operator ==(Ulamek valueA, Ulamek valueB)
+        {
+            if (valueA.ToString() == valueB.ToString())
+                return true;
+            else
+                return false;
+        }
+        static int ZnajdzGCD(int a, int b)
+        {
+            while (b != 0)
+            {
+                int temp = b;
+                b = a % b;
+                a = temp;
+            }
+            return a;
+        }
+        static int ZnajdzLCM(int a, int b)
+        {
+            return (a * b) / ZnajdzGCD(a, b);
+        }
+    }
+}
